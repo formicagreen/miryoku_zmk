b1af780cb7b1ed9e2cbb3edc3c91b9808e678cc4
Shantiram <sr@yogameditation.com>
2023-06-29 08:54:26 +0200

    asdfasdf

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,8 +1,6 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
-#pragma once
-
 #if !defined (MIRYOKU_LAYOUTMAPPING_CORNE)
 
 #define XXX &none
@@ -13,10 +11,10 @@
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-&kp C_VOL_UP  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  U_LT(U_NUMPAD, LA(W)), \
+&kp C_VOL_UP  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &kp LA(W) \
 &kp C_VOL_DN  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &kp LA(A) \
 &kp C_MUTE    K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp LA(O) \
-                        K32  K33  K34       K35  K36  K37
+               K32  K33  K34       K35  K36  K37
 
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTPINKIE 2 3
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTINNERINDEX 3 4
@@ -30,5 +28,6 @@
 
 #define MIRYOKU_KLUDGE_THUMBCOMBOS_LEFT 37 38
 #define MIRYOKU_KLUDGE_THUMBCOMBOS_RIGHT 39 40
+
 
 #endif
