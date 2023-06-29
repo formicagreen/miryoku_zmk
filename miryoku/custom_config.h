69f9777b4d46377cad8bef8e4c8388ab0668eb73
Shantiram <sr@yogameditation.com>
2023-06-29 08:48:47 +0200

    various remove underglow increase signal strength remove failing code

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -16,7 +16,7 @@
 &kp C_VOL_UP  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  U_LT(U_NUMPAD, LA(W)), \
 &kp C_VOL_DN  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &kp LA(A) \
 &kp C_MUTE    K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp LA(O) \
-               K32  K33  K34       K35  K36  K37
+                        K32  K33  K34       K35  K36  K37
 
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTPINKIE 2 3
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTINNERINDEX 3 4
@@ -32,37 +32,3 @@
 #define MIRYOKU_KLUDGE_THUMBCOMBOS_RIGHT 39 40
 
 #endif
-
-#define MIRYOKU_LAYER_LIST \
-MIRYOKU_X(BASE,   "Base") \
-MIRYOKU_X(EXTRA,  "Extra") \
-MIRYOKU_X(TAP,    "Tap") \
-MIRYOKU_X(BUTTON, "Button") \
-MIRYOKU_X(NAV,    "Nav") \
-MIRYOKU_X(MOUSE,  "Mouse") \
-MIRYOKU_X(MEDIA,  "Media") \
-MIRYOKU_X(NUM,    "Num") \
-MIRYOKU_X(SYM,    "Sym") \
-MIRYOKU_X(FUN,    "Fun") \
-MIRYOKU_X(NUMPAD,    "NumPad")
-
-#define U_BASE   0
-#define U_EXTRA  1
-#define U_TAP    2
-#define U_BUTTON 3
-#define U_NAV    4
-#define U_MOUSE  5
-#define U_MEDIA  6
-#define U_NUM    7
-#define U_SYM    8
-#define U_FUN    9
-#define U_NUMPAD 10
-
-
-#define MIRYOKU_LAYER_NUMPAD \
-&kp LBKT,          &kp KP_N7,            &kp KP_N8,            &kp KP_N9,            &kp RBKT,          U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
-&kp SEMI,          &kp KP_N4,            &kp KP_N5,            &kp KP_N6,            &kp EQUAL,         U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
-&kp GRAVE,         &kp KP_N1,            &kp KP_N2,            &kp KP_N3,            &kp BSLH,          U_NA,              &u_to_U_NUM,       &u_to_U_NAV,       &kp RALT,          U_NA,              \
-U_NP,              U_NP,                 &kp DOT,              &kp KP_N0,            &kp MINUS,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP
-
-#define MIRYOKU_LAYERMAPPING_NUMPAD MIRYOKU_MAPPING
